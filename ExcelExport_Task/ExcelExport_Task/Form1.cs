43d75e97428463df25568a35555f36fe7a8f547f
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-10-01 20:42:23 +0200

    Create Excel&Error handling

--- ExcelExport_Task/ExcelExport_Task/Form1.cs
+++ ExcelExport_Task/ExcelExport_Task/Form1.cs
@@ -16,13 +16,37 @@ namespace ExcelExport_Task
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Sheets xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
         private void LoadData() {
             Flats = context.Flats.ToList();
+        }
+        private void CreateExcel() {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWb.ActiveSheet;
+                //CreateTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}",ex.Message,ex.Source);
+                MessageBox.Show(errMsg,"Error");
+                xlWb.Close(false,Type.Missing,Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
         }
     }
 }
