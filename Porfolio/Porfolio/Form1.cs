d73ba92d5f45658ce563b87299b6fa3591606ba0
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-10-07 20:28:24 +0200

    Portfolioertekszamitas

--- Porfolio/Porfolio/Form1.cs
+++ Porfolio/Porfolio/Form1.cs
@@ -30,5 +30,16 @@ namespace Porfolio
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
             dataGridView2.DataSource = Portfolio;
         }
+        private decimal GetPortfolioValue(DateTime date) {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in ticks 
+                            where item.Index == x.Index.Trim()
+                            && date<=x.TradingDay select x).First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
