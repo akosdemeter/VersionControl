bccf31eac2d7a3e64d7e9a674e50f5f2e14470cf
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-10-07 20:19:50 +0200

    Portfolio

--- Porfolio/Porfolio/Form1.cs
+++ Porfolio/Porfolio/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Porfolio.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,11 +15,20 @@ namespace Porfolio
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             ticks = context.Ticks.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+        }
+        private void CreatePortfolio() {
+            Portfolio.Add(new PortfolioItem() {Index="OTP",Volume=10});
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
