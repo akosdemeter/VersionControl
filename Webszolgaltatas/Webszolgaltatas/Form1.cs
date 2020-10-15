68d45fa9d6e52e60dde5dd1326ea4cf06630eeb7
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-10-15 22:17:57 +0200

    Xmlfeldolgoz

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Webszolgaltatas.Entities;
 using Webszolgaltatas.MnbServiceReference;
 
@@ -18,10 +19,11 @@ namespace Webszolgaltatas
         public Form1()
         {
             InitializeComponent();
-            WebSzolg();
             dataGridView1.DataSource = rates;
+            GettingResults();
+            XmlFeldolgoz();
         }
-        public void WebSzolg() {
+        public void GettingResults() {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
@@ -31,6 +33,27 @@ namespace Webszolgaltatas
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateDate();
+                rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childelement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childelement.GetAttribute("curr");
+                var unit = decimal.Parse(element.GetAttribute("unit"));
+                var value = decimal.Parse(element.InnerText);
+                if (unit!=0)
+                {
+                    rate.Value = value / unit;
+                }
+            }
+        }
+        public void XmlFeldolgoz() {
+            
         }
     }
 }
