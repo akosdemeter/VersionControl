fe6356985c5b650576649b2467ff1cb635ffd0f9
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-10-15 21:50:10 +0200

    webszolg hívása

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas.MnbServiceReference;
 
 namespace Webszolgaltatas
 {
@@ -15,6 +16,18 @@ namespace Webszolgaltatas
         public Form1()
         {
             InitializeComponent();
+            WebSzolg();
+        }
+        public void WebSzolg() {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
