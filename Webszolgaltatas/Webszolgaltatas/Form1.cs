0dc54c9827e247a021840b50176744cd286bf89b
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-10-15 21:59:33 +0200

    árfolyamlista

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,16 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas.Entities;
 using Webszolgaltatas.MnbServiceReference;
 
 namespace Webszolgaltatas
 {
     public partial class Form1 : Form
     {
+        BindingList<RateDate> rates = new BindingList<RateDate>();
         public Form1()
         {
             InitializeComponent();
             WebSzolg();
+            dataGridView1.DataSource = rates;
         }
         public void WebSzolg() {
             var mnbService = new MNBArfolyamServiceSoapClient();
