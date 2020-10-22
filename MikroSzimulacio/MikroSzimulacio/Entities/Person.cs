1f63f4be89062d895a340cfa7a26e86bb6af01c3
Ákos Demeter <akos.demeter@stud.uni-corvinus.hu>
2020-10-22 20:15:09 +0200

    Classes

---
+++ MikroSzimulacio/MikroSzimulacio/Entities/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikroSzimulacio.Entities
+{
+    public class Person
+    {
+        public int BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+        public Person() {
+            IsAlive = true;
+        }
+    }
+}
